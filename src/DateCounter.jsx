25d53af14a558c035114530349f6581c630d714c
SouzaMVictor <souzamvictor@gmail.com>
2025-02-19 19:19:43 -0300

    reducer explanation

--- src/DateCounter.jsx
+++ src/DateCounter.jsx
@@ -1,14 +1,15 @@
 import "./App.css";
-
+//no fim do arquivo tem uma explicação ludica de como funciona
 import { useReducer } from "react";
-
+//modo alternativo de settar o state. ideal para aplicaçãoes complexas
 const initialState = { count: 0, step: 1 };
+//initial state pode ser um valor primitivo, mas geralmente é um objeto.
 function reducer(state, action) {
-  console.log(state, action);
-  // if (action.type === "inc") return state + 1;
-  // if (action.type === "dec") return state - 1;
-  // if (action.type === "setCount") return action.payload;
+  //reducer function
+  //os parametros guardam states num objeto
+  //action descreve como o state deve ser atualizado
   switch (action.type) {
+    //não pode levar side effects. tira a logica de state do componente, deixando mais limpo
     case "dec":
       return { ...state, count: state.count - state.step };
     case "inc":
@@ -26,6 +27,8 @@ function reducer(state, action) {
 
 function DateCounter() {
   const [state, dispatch] = useReducer(reducer, initialState);
+  //state guarda o object
+  //dispatch é a função que vai fazer o state atualizar, por mandar as ações dos event handlers pro reducer. usado ao inves do setState()
   const { count, step } = state;
 
   // This mutates the date object.
@@ -34,6 +37,7 @@ function DateCounter() {
 
   const dec = function () {
     dispatch({ type: "dec" });
+    //coordena o state e passa pra função reducer
   };
 
   const inc = function () {
@@ -80,3 +84,13 @@ function DateCounter() {
   );
 }
 export default DateCounter;
+//situação de mundo real usando os conceitos de dispatcher,reducer e state.
+//vc quer pegar 5 mil reais da conta do banco. como faz isso? vai direto no cofre pegar os 5 mil e sai do banco? não.
+//vc vai falar com o bancario e pedir 5 mil da conta 12435. entao, o bancario vai checar sua conta e ver se tem isso mesmo e depois te passa o dinheiro do cofre.
+//na suposição, vc é o dispatcher(que vai pedir pra modificar o state)
+//o bancario é o reducer(que vai modificar o state)
+//o cofre é o state, que vai ter o conteudo modificado
+// EXEMPLO:
+//type:"withdraw",
+// payload:
+//  {amount:5000, account:12435}
